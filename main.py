2d93e547ae76d15e44cc6d1a47b7f29841a14712
BhusterEby <aiden.eby@gmail.com>
2025-09-03 20:21:27 -0500

    3- updated 'main.py' with the ability to close screen/program without using Ctrl-C

--- main.py
+++ main.py
@@ -6,11 +6,23 @@ import pygame
 from constants import *
 
 def main():
+    # initialize the pygame import
     pygame.init()
+    # sets up the screen using the static variables from the constants import
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    # the game loop; True refers to "while the game is on"
     while True:
+        # for each event that occurs per iteration
+        for event in pygame.event.get():
+            # check if the action of quitting the game has commensed
+            if event.type == pygame.QUIT:
+                # a blank return, assumed to end the game loop, and thus stop the program without having to use Ctrl-C
+                return
+        # this fills the screen with a black background (0 red, 0 blue, 0 green)
         screen.fill((0, 0, 0))
+        # display.flip refreshes the screen; should always be called last
         pygame.display.flip()
+    
     # print("Starting Asteroids!")
     # print(f"Screen width: {SCREEN_WIDTH}")
     # print(f"Screen height: {SCREEN_HEIGHT}")
