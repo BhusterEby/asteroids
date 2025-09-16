0f562e0f5e4fa8395a4766fb4122f4b4c30c5437
BhusterEby <aiden.eby@gmail.com>
2025-09-15 19:05:47 -0500

    9- updated 'main.py' to use groups for updating and rendering everything (also tidied up some code)

--- main.py
+++ main.py
@@ -9,16 +9,28 @@ from circleshape import *
 from player import *
 
 def main():
+    # ==INITIALIZING==
     # initialize the pygame import
     pygame.init()
     # creating a clock object
     game_clock = pygame.time.Clock()
     # set up the delta variable
     dt = 0
+
+    # ==MAIN SETUPS==
     # sets up the screen using the static variables from the constants import
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    # creating a group for all objects requiring updates
+    update_group = pygame.sprite.Group()
+    # creating a group for all objects that get drawn
+    draw_group = pygame.sprite.Group()
+    Player.containers = (update_group, draw_group)
+
+    # ==SPECIAL SETUPS==
     # setting up the player
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    # ==GAMEPLAY LOOP==
     # the game loop; True refers to "while the game is on"
     while True:
         # for each event that occurs per iteration
@@ -29,13 +41,15 @@ def main():
                 return
         # this fills the screen with a black background (0 red, 0 blue, 0 green)
         screen.fill((0, 0, 0))
-        # update the player's position [ROTATION]
-        player.update(dt)
-        # render the player
-        player.draw(screen)
+        # update everything
+        update_group.update(dt)
+        # render everything using a loop
+        for item in draw_group:
+            # render this item
+            item.draw(screen)
         # display.flip refreshes the screen; should always be called last
         pygame.display.flip()
-        # calling the .tick() method, setting fps to 60, and translating that time to seconds and capturing it in dt
+        # calling the .tick() method, setting fps to 60, and translating that time (from ms to s) and capturing it in dt
         dt = game_clock.tick(60) / 1000
 
 if __name__ == "__main__":
